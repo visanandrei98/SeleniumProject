9c525faaa517971086d4262933dfa2c92936c569
Visan Andrei <48958844+visanandrei98@users.noreply.github.com>
2025-07-22 19:15:11 +0300

    Testing Dates of format mm/dd/yyyy
    Creating a test to autocomplete date, month and year.

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -7,11 +7,18 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 
 public class WidgetsPage extends HomePage {
     private By selectMenuItem = By.xpath("//li[@id='item-8']/span[text()='Select Menu']");
+    private By datePickerMenu = By.xpath("//li[@id='item-2']/span[text()='Date Picker']");
 
 
     public SelectMenuPage clickSelectMenu(){
         scrollToElementJS(selectMenuItem);
         click(selectMenuItem);
         return new SelectMenuPage();
+    }
+
+    public DatePickerMenuPage clickDatePicker(){
+        scrollToElementJS(datePickerMenu);
+        click(datePickerMenu);
+        return new DatePickerMenuPage();
     }
 }
